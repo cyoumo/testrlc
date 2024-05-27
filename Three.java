600d00e6b253d5a99a742db6f4747627f4bdad85
zmeng <zmeng581@gmail.com>
2024-05-27 16:32:22 +0900

    22日

--- Three.java
+++ Three.java
@@ -5,7 +5,7 @@ public class Three {
 		int[] arr = new int[31];
 		int sum=0;
         for (int i = 20; i < 51; i++) {
-        	if(i%3 == 0) {
+        	if(i%3 == 1) {
         		sum = sum + i;
         	}
         	
