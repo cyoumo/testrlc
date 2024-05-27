98e96c38dce515bf9bbc28f48788bb9c77056564
ProfessorRLC <rlc643765521@gmail.com>
2024-05-27 16:31:12 +0900

    Add files via upload

---
+++ Three.java
@@ -0,0 +1,69 @@
+package com.ogm;
+
+public class Three {
+	public static void main(String[] args) {
+		int[] arr = new int[31];
+		int sum=0;
+        for (int i = 20; i < 51; i++) {
+        	if(i%3 == 0) {
+        		sum = sum + i;
+        	}
+        	
+        	}
+        System.out.println(sum);
+        }
+        }
+        
+ 
+      
+    
+
+	/*
+		int[] arr = new int[30]; 
+        for (int i = 0; i < arr.length; i++) {
+           
+        	int number;
+        	
+        	
+
+            while (number > 50 || number < 20); // 确保数字在20到50之间
+            {
+            arr[number] = number; // 将限定范围内的数字存储到数组中
+        }
+        
+	}
+	}
+}
+ 
+            /*
+        // 打印数组元素
+        for (int  : arr) {
+            System.out.println(i);
+        }
+    }
+
+		
+	/*
+		int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = generateRandomNumber();
+        }
+        return arr;
+    
+ 
+	/*
+    private static int generateRandomNumber() {
+        return (int)(Math.sum() * 100) + 1; // 生成1到100之间的随机数
+    }
+ 
+    public static void main(String[] args) {
+        int[] boundedArray = createBoundedArray(10); // 创建一个长度为10的数组
+        for (int number : boundedArray) {
+            System.out.println(number);
+        }
+    }
+    */
+
+		
+		
+
